becf419849de391b4a995da2d0064b25752d823c
JEONGBEOM KO <beom9807@gmail.com>
2021-12-16 23:17:22 +0900

    SpringBoot-tutorial
    스프링 입문-스프링 부트

---
+++ hello-spring/src/test/java/hello/hellospring/HelloSpringApplication.java
@@ -0,0 +1,20 @@
+package hello.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+class HelloSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringApplication.class, args);
+    }
+
+
+//	@Test
+//	void contextLoads() {
+//	}
+}
+
